566294336644b3181779a69368283e2d2f252ebf
Maxim <maximbotin7@yandex.ru>
2021-07-11 20:09:43 +0300

    init

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -9,9 +9,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
-
-    @Bean
-    public static Island2 getIsland(Wood3 wood) {
-        return new Island2(wood);
-    }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,26 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    @Autowired
+    private Needle7 needle7;
+
+    @Autowired
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
